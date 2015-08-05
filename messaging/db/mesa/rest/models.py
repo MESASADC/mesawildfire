3b7134001d5e68b5fd0839b4db43644cdab3ddb3
Riaan van den Dool <rvddool@csir.co.za>
2015-08-05 21:09:33 +0200

    Busy with django models

--- messaging/db/mesa/rest/models.py
+++ messaging/db/mesa/rest/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class AfModis(models.Model):
+    type = models.CharField(blank=True, default='')
+    geom = models.PointField()
+    lon = models.DecimalField()
+    lat = models.DecimalField()
+    date_time = models.DateTimeField()
+    
+
