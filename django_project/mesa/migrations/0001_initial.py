01fea6dcf4f9c48be797a9ba7d3b9cb2cd01a194
Riaan van den Dool <rvddool@csir.co.za>
2015-08-25 16:01:41 +0200

    Data fixtures LFDI gif files FDITable view

--- django_project/mesa/migrations/0001_initial.py
+++ django_project/mesa/migrations/0001_initial.py
@@ -97,3 +97,62 @@ class Migration(migrations.Migration):
             )],
         ),
     ]
+    
+    fdi_table_select = """
+        SELECT 
+          mesa_fdipoint.id, 
+          mesa_fdipoint.name, 
+          mesa_fdipoint.type, 
+          mesa_fdipoint.station_name, 
+          mesa_fdimeasurement.fdi_value, 
+          mesa_fdimeasurement.fdi_rgb, 
+          mesa_fdipoint.point, 
+          mesa_fdipoint.lon, 
+          mesa_fdipoint.lat, 
+          mesa_fdimeasurement.rain_mm, 
+          mesa_fdimeasurement.windspd_kmh, 
+          mesa_fdimeasurement.winddir_deg, 
+          mesa_fdimeasurement.rh_pct, 
+          mesa_fdimeasurement.temp_c, 
+          mesa_fdimeasurement.date_time
+        FROM 
+          public.mesa_fdipoint, 
+          public.mesa_fdimeasurement
+        WHERE 
+          mesa_fdimeasurement.fdi_point_id = mesa_fdipoint.id;
+    """
+
+ 
+    operations += [
+        migrations.RunSQL(
+            sql="CREATE OR REPLACE VIEW mesa_fditable AS %s;" % fdi_table_select,
+            reverse_sql='DROP VIEW IF EXISTS mesa_fditable CASCADE;',
+            state_operations=[migrations.CreateModel(
+                name='FdiTable',
+                fields=[
+                    ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                    ('name', models.CharField(unique=True, max_length=40)),
+                    ('type', models.CharField(default=b'poi', max_length=20, blank=True, choices=[(b'wstation', b'Weather station'), (b'poi', b'Point of interest')])),
+                    ('station_name', models.CharField(default=None, max_length=40, unique=True, null=True, blank=True)),
+                    ('station_id', models.CharField(default=None, max_length=40, unique=True, null=True, blank=True)),
+                    ('fdi_value', models.IntegerField()),
+                    ('fdi_rgb', models.CharField(default=b'', max_length=10, blank=True)),
+                    ('point', django.contrib.gis.db.models.fields.PointField(srid=4326)),
+                    ('lon', models.FloatField()),
+                    ('lat', models.FloatField()),
+                    ('rain_mm', models.FloatField(null=True, blank=True)),
+                    ('windspd_kmh', models.FloatField()),
+                    ('winddir_deg', models.FloatField()),
+                    ('rh_pct', models.FloatField()),
+                    ('temp_c', models.FloatField()),
+                    ('date_time', models.DateTimeField(null=True, blank=True)),
+                ],
+                options={
+                    'abstract': False,
+                    'managed': False,
+                },
+            )],
+        )
+    
+    ]
+
