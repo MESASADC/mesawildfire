9e00ad722cd5e46e1b4e5b7603a307bb636bb6df
Riaan van den Dool <rvddool@csir.co.za>
2015-10-27 09:29:44 +0200

    Merged from master

--- admin/supervisord/conf.d/mesa_web.sh
+++ admin/supervisord/conf.d/mesa_web.sh
@@ -9,13 +9,13 @@ docker rm -f supervisor_mesa_web &> /dev/null
 echo "Make sure Postgresql and RabbitMQ is up and accepting connections:"
 docker run --link supervisor_postgis --rm martin/wait
 docker run --link supervisor_rabbitmq --rm martin/wait
-docker run --link supervisor_geoserver --rm martin/wait
+docker run --link supervisor_geonode --rm martin/wait
 
 echo "Give PostGIS docker time to start up"
 sleep 20
 
 echo "Start django:"
-docker run -t --rm -a stdout -a stderr --name supervisor_mesa_web --link supervisor_geoserver:geoserver --link supervisor_postgis:postgis --link supervisor_rabbitmq:rabbitmq -v $MESA_ROOT/django_project/ENV:/ENV -v $MESA_ROOT/django_project/start.sh:/start.sh -v $MESA_ROOT/django_project:/django_project -v $MESA_ROOT/volumes/web_static:/static_root/ -p 8112:8000 mesa_django
+docker run -t --rm -a stdout -a stderr --name supervisor_mesa_web --link supervisor_geonode:geoserver --link supervisor_postgis:postgis --link supervisor_rabbitmq:rabbitmq -v $MESA_ROOT/django_project/ENV:/ENV -v $MESA_ROOT/django_project/start.sh:/start.sh -v $MESA_ROOT/django_project:/django_project -v $MESA_ROOT/volumes/web_static:/static_root/ -p 8112:8000 mesa_django
 
 RESULT=$?
 
--- django_project/mesa/dashboard/views.py
+++ django_project/mesa/dashboard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render_to_response
 def dashboard(request):
     return render_to_response('dashboard.html')
 
+def wmst(request):
+    return render_to_response('wmst.html')
+
