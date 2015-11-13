a57bc11ca31d31afc680735589d41960c8f7e985
Riaan van den Dool <rvddool@csir.co.za>
2015-11-13 14:47:42 +0200

    Dump GeoNode, revert to Geoserver. GeoNode seems buggy and not Africa proof. When uploading a shapefile, press clear button. Now in a state that wont allow upload!

--- admin/supervisord/conf.d/nginx.sh
+++ admin/supervisord/conf.d/nginx.sh
@@ -10,11 +10,11 @@ docker rm -f supervisor_nginx &> /dev/null
 
 # Make sure Geoserver, Django and RabbitMQ is up and accepting connections:
 docker run --link supervisor_rabbitmq --rm martin/wait
-docker run --link supervisor_geonode --rm martin/wait
+docker run --link supervisor_geoserver --rm martin/wait
 docker run --link supervisor_mesa_web --rm martin/wait
 
 # Start django:
-docker run -t --rm -a stdout -a stderr --name supervisor_nginx --link supervisor_geonode:geoserver --link supervisor_mesa_web:mesa_web --link supervisor_rabbitmq:rabbitmq -p 80:80 -v $MESA_ROOT/nginx/sites:/etc/nginx/sites-enabled -v $MESA_ROOT/nginx/www:/var/www/ mesa/nginx
+docker run -t --rm -a stdout -a stderr --name supervisor_nginx --link supervisor_geoserver:geoserver --link supervisor_mesa_web:mesa_web --link supervisor_rabbitmq:rabbitmq -p 80:80 -v $MESA_ROOT/nginx/sites:/etc/nginx/sites-enabled -v $MESA_ROOT/nginx/www:/var/www/ mesa/nginx
 
 RESULT=$?
 
