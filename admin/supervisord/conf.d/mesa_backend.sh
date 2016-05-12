f2ad8b031c34954f3e4e6eb42fa8ba79bacd153f
Steve Masha <smasha@csir.co.za>
2016-05-12 13:06:05 +0100

    ENV file is located in admin/ not django_project/

--- admin/supervisord/conf.d/mesa_backend.sh
+++ admin/supervisord/conf.d/mesa_backend.sh
@@ -12,7 +12,7 @@ docker run --link supervisor_postgis --rm martin/wait
 docker run --link supervisor_rabbitmq --rm martin/wait -p 5672,61613,80,15672,15674  # dont wait for port 443
 
 # Start django:
-docker run -t --rm -a stdout -a stderr --name supervisor_mesa_backend --link supervisor_postgis:postgis --link supervisor_rabbitmq:rabbitmq -v $MESA_ROOT/django_project/ENV:/ENV -v $MESA_ROOT/django_project/start.sh:/start.sh -v $MESA_ROOT/django_project:/django_project mesasadc/mesa_django:$BUILD_TAG /django_project/manage.py mesa_comms
+docker run -t --rm -a stdout -a stderr --name supervisor_mesa_backend --link supervisor_postgis:postgis --link supervisor_rabbitmq:rabbitmq -v $MESA_ROOT/admin/ENV:/ENV -v $MESA_ROOT/django_project/start.sh:/start.sh -v $MESA_ROOT/django_project:/django_project mesasadc/mesa_django:$BUILD_TAG /django_project/manage.py mesa_comms
 
 RESULT=$?
 
